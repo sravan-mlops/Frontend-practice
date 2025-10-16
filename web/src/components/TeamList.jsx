3730a1db705b238e7cec6b6466ffad13f5e7c245
sravan <sravandevops903@gmail.com>
2025-10-16 17:44:44 +0530

    team list changed

--- web/src/components/TeamList.jsx
+++ web/src/components/TeamList.jsx
@@ -3,30 +3,42 @@ import { useState } from "react";
 import TeamMember from "./TeamMember";
 import "./TeamList.css";
 
-
-
 const teamMembers = [
-  { id: 1, name: "Alice", role: "Frontend Developer", img: "https://i.pravatar.cc/150?img=1" },
-  { id: 2, name: "Bob", role: "Backend Developer", img: "https://i.pravatar.cc/150?img=2" },
-  { id: 3, name: "Charlie", role: "UI/UX Designer", img: "https://i.pravatar.cc/150?img=3" },
+  {
+    id: 1,
+    name: "Alice",
+    role: "Frontend Developer",
+    img: "https://i.pravatar.cc/150?img=1",
+  },
+  {
+    id: 2,
+    name: "Bob",
+    role: "Backend Developer",
+    img: "https://i.pravatar.cc/150?img=2",
+  },
+  {
+    id: 3,
+    name: "sravan",
+    role: "UI/UX Designer",
+    img: "https://i.pravatar.cc/150?img=3",
+  },
 ];
 
 const TeamList = () => (
   <div>
     <h2>OUR TEAM</h2>
     <div>
-      {teamMembers.map(member=> (
+      {teamMembers.map((member) => (
         <TeamMember
-        key={member.id}
-        name={member.name}
-        role={member.role}
-        img={member.img}
+          key={member.id}
+          name={member.name}
+          role={member.role}
+          img={member.img}
         />
       ))}
     </div>
   </div>
 );
-
 
 // function TeamList(){
 //     const[showTeam, setShowTeam] = useState(true);
